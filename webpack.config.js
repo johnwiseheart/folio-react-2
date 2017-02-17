f84fd9f6b666055c3fad22d85b7390452dff28a6
John Wiseheart <johnwiseheart@gmail.com>
2017-02-17 22:11:38 +1100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var webpack = require('webpack');
+var path = require('path');
+var WebpackNotifierPlugin = require('webpack-notifier');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    // Add the react hot loader entry point - in reality, you might only want this in your dev config
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    'index.tsx'
+  ],
+  output: {
+    filename: 'app.js',
+    publicPath: '/dist',
+    path: path.resolve('dist')
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    modules: ['src', 'node_modules'],
+  },
+  module: {
+    loaders: [
+      // .ts(x) files should first pass through the Typescript loader, and then through babel
+      { test: /\.tsx?$/, loaders: ['babel-loader', 'ts-loader'] }
+    ]
+  },
+  plugins: [
+    // Add the HMR plugin
+    new webpack.HotModuleReplacementPlugin(),
+    new WebpackNotifierPlugin({ alwaysNotify: true }),
+  ]
+};
